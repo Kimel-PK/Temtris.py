1001e6e4ae5c3e246d96a6d244aa01baf7a41965
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2022-11-15 18:56:13 +0100

    Rozbijanie linii, tryb dla dwóch graczy

--- Temtris.py
+++ Temtris.py
@@ -1,3 +1,26 @@
+# #####################################################################################################
+# 
+# ┌──────────────────┐                                    ┌──┐
+# └───────┐  ┌───────┘                                    │  │
+#         │  │                                       ─────┘  └──┐                 ┌──┐
+#         │  │                                     /──────┐  ┌──┘                 └──┘
+#         │  │    ┌──────────┐                            │  │
+#         │  │    │  ┌───────┘    ┌────────────────       │  │    |\    ─────┐    ┌──┐      ────────
+#         │  │    │  │            │  ┌────┐  ┌────┐ \     │  │    │ \ /   ───┘    │  │    /   ───────\
+#         │  │    │  └───┐        │  │    │  │    │  │    │  │    │     /         │  │     \  \
+#         │  │    │  ┌───┘        │  │    │  │    │  │    │  │    │   /           │  │        \  \
+#         │  │    │  │            │  │    │  │    │  │    │  │    │  │            │  │           \  \
+#         │  │    │  └───────┐    │  │    │  │    │  │    │  │    │  │            │  │     ─────     /
+#         │  │    └──────────┘    └──┘    └──┘    └──┘    └──┘    └──┘            └──┘     \────────/
+#         │ /
+#         │/
+# 
+# #####################################################################################################
+#                                                       Port Temtris 1.5.4 (NES) => Temtris.py (Python)
+#                                              https://github.com/Kimel-PK/Temtris
+#                                                                                              Kimel_PK
+# #####################################################################################################
+
 import pygame
 from pygame.locals import *
 from random import randrange
@@ -13,7 +36,6 @@ PX = 4 # szerokość piksela
 PY = 4 # wysokość piksela
 X = 32 # szerokość sprite
 Y = 32 # wysokość sprite
-klatka = 16.66 # czas trwania klatki w milisekundach
 
 # kontroler NES
 # gracz 1
@@ -39,7 +61,35 @@ SELECT2 = K_RSHIFT
 pygame.init()
 zegar = pygame.time.Clock ()
 okno = pygame.display.set_mode ((szerokośćOkna, wysokośćOkna))
+# TODO zrobić favicona
 pygame.display.set_caption ("Temtris.py")
+
+pauza = False
+poziom = 0
+liczbaLinii = 0
+liczbaLiniiCheems = 0
+liczbaLiniiDoge = 0
+liczbaLiniiBuffDoge = 0
+liczbaLiniiTemtris = 0
+czasGry = 0
+obecnyGracz = 0
+zegarKontrolera = 10
+szybkośćOpadaniaKlocka = 60
+zegarOpadania = 60
+
+uderzenieSFX = pygame.mixer.Sound("Assets/Dźwięk/uderzenie.ogg")
+
+cheemsSFX = pygame.mixer.Sound("Assets/Dźwięk/cheems.ogg")
+dogeSFX = pygame.mixer.Sound("Assets/Dźwięk/doge.ogg")
+buffdogeSFX = pygame.mixer.Sound("Assets/Dźwięk/buffdoge.ogg")
+temtrisSFX = pygame.mixer.Sound("Assets/Dźwięk/temtris.ogg")
+
+muzyka = [
+	"Assets/Dźwięk/Never Gonna Give You Up NES APU cover.ogg",
+	"Assets/Dźwięk/Together Forever NES APU cover.ogg",
+	"Assets/Dźwięk/Song For Denise NES APU cover.ogg",
+	"Assets/Dźwięk/Szanty Bitwa NES APU cover.ogg"
+]
 
 # INTRO
 
@@ -53,7 +103,6 @@ def Intro () :
 			if event.type == KEYDOWN :
 				# START
 				if event.key == (START1 or START2) :
-					print ("START")
 					return
 		
 		pygame.display.update ()
@@ -75,7 +124,6 @@ def Intro () :
 				if event.type == KEYDOWN :
 					# START
 					if event.key == (START1 or START2) :
-						print ("START")
 						return
 			
 			pygame.display.update ()
@@ -87,7 +135,6 @@ def Intro () :
 			if event.type == KEYDOWN :
 				# START
 				if event.key == (START1 or START2) :
-					print ("START")
 					return
 		
 		pygame.display.update ()
@@ -179,8 +226,6 @@ class Klocek (pygame.sprite.Sprite) :
 		self.x = x - 10
 		self.y = y - 5
 		
-		print ("ustawianie pozycji na: (" + str (self.x) + ", " + str (self.y) + ")")
-		
 		self.rect.x = x * X
 		self.rect.y = y * Y
 		
@@ -214,8 +259,6 @@ class Klocek (pygame.sprite.Sprite) :
 	def Opadaj (self) :
 		
 		if Klocek.plansza.SprawdźKolizję (self.UtwórzMacierzKolizji (self.image), self.x, self.y + 1) :
-			
-			print ("kolizja, umieść klocek")
 			return False
 		else :
 			self.rect.y += Y
@@ -225,49 +268,41 @@ class Klocek (pygame.sprite.Sprite) :
 	
 	def PrzesuńWPrawo (self) :
 		
-		if Klocek.plansza.SprawdźKolizję (self.UtwórzMacierzKolizji (self.image), self.x + 1, self.y) :
-			
-			print ("kolizja, nie można przesunąć")
-			
-		else :
+		if not Klocek.plansza.SprawdźKolizję (self.UtwórzMacierzKolizji (self.image), self.x + 1, self.y) :
 			self.rect.x += X
 			self.x += 1
 		
 	def PrzesuńWLewo (self) :
 		
-		if Klocek.plansza.SprawdźKolizję (self.UtwórzMacierzKolizji (self.image), self.x - 1, self.y) :
-			print ("kolizja, nie można przesunąć")
-		else :
+		if not Klocek.plansza.SprawdźKolizję (self.UtwórzMacierzKolizji (self.image), self.x - 1, self.y) :
 			self.rect.x -= X
 			self.x -= 1
 	
 	def ObróćWPrawo (self) :
 		
+		# TODO obrót z przesunięciem
+		
 		obrót = (self.obrót + 1) % 4
-		print ("obrót: " + str (self.obrót))
 		
 		macierzKolizji = self.UtwórzMacierzKolizji (self.UtwórzGrafike (obrót, self.numerKlocka))
 		
 		self.image = self.UtwórzGrafike (self.obrót, self.numerKlocka)
 		
-		if Klocek.plansza.SprawdźKolizję (macierzKolizji, self.x, self.y) :
-			print ("kolizja, nie można przesunąć")
-		else :
+		if not Klocek.plansza.SprawdźKolizję (macierzKolizji, self.x, self.y) :
 			self.obrót = (self.obrót + 1) % 4
 			self.image = self.UtwórzGrafike (self.obrót, self.numerKlocka)
 		
 	def ObróćWLewo (self) :
 		
+		# TODO obrót z przesunięciem
+		
 		obrót = (self.obrót - 1) % 4
-		print ("obrót: " + str (self.obrót))
 		
 		macierzKolizji = self.UtwórzMacierzKolizji (self.UtwórzGrafike (obrót, self.numerKlocka))
 		
 		self.image = self.UtwórzGrafike (self.obrót, self.numerKlocka)
 		
-		if Klocek.plansza.SprawdźKolizję (macierzKolizji, self.x, self.y) :
-			print ("kolizja, nie można przesunąć")
-		else :
+		if not Klocek.plansza.SprawdźKolizję (macierzKolizji, self.x, self.y) :
 			self.obrót = (self.obrót - 1) % 4
 			self.image = self.UtwórzGrafike (self.obrót, self.numerKlocka)
 
@@ -296,13 +331,36 @@ class Fragment (pygame.sprite.Sprite) :
 		grafika.set_colorkey ((0, 0, 0))
 		
 		self.image = grafika
+
+class AnimacjaRozbijanejLinii (pygame.sprite.Sprite) :
 	
+	rozbijanieLiniiSprite = pygame.image.load ("Assets/Grafika/rozbijanie linii sprite.png").convert_alpha ()
+	
+	def __init__ (self, ilośćLinii, numerLinii) :
+		super ().__init__ ()
+		
+		self.klatka = -1
+		self.ilośćLinii = ilośćLinii
+		
+		self.NastępnaKlatka ()
+		self.rect = self.image.get_rect ()
+		self.rect.x = 10 * X
+		self.rect.y = 5 * Y + numerLinii * Y
+	
+	def NastępnaKlatka (self) :
+		
+		self.klatka += 1
+		
+		grafika = pygame.Surface((320, 32)).convert_alpha ()
+		grafika.blit (AnimacjaRozbijanejLinii.rozbijanieLiniiSprite, Rect (0, 0, 320, 32), Rect ((self.ilośćLinii - 1) * 320, self.klatka * 32, 320, 32))
+		grafika.set_colorkey ((0, 255, 0))
+		
+		self.image = grafika
+		
 class Plansza () :
 	
 	def __init__ (self) :
-		
 		self.macierzSpriteów = [[None for x in range(10)] for y in range(20)]
-		print (self.macierzSpriteów)
 		
 	def Rysuj (self) :
 		
@@ -332,28 +390,22 @@ class Plansza () :
 		
 	def SprawdźKolizję (self, macierzKolizji, x, y) :
 		
-		print ("sprawdzanie kolizji w punkcie (" + str (x) + ", " + str (y) + ")")
-		
 		for sy in range (0, 4) :
 			for sx in range (0, 4) :
 				
-				# fragment klocka wyszedł za plansze, czyli mamy kolizję ze ścianą
-				if macierzKolizji[sy][sx] == 1 and (x + sx < 0 or x + sx > 9 or y + sy > 19) :
-					print ("kolizja wyjście klocka za plansze")
-					return True
-				
+				# wyszliśmy po za planszę
 				if x + sx < 0 or x + sx > 9 or y + sy > 19 :
+					# fragment klocka wyszedł za plansze, czyli mamy kolizję ze ścianą
+					if macierzKolizji[sy][sx] == 1 :
+						return True
 					continue
 				
 				if self.macierzSpriteów[y + sy][x + sx] != None and macierzKolizji[sy][sx] == 1 :
 					return True
 		
-		print ("brak kolizji")
 		return False
 	
 	def UmieśćKlocek (self, macierzKolizji, x, y) :
-		
-		print ("umieść klocek na (" + str (x) + "," + str (y) + ")")
 		
 		for sy in range (0, 4) :
 			for sx in range (0, 4) :
@@ -379,34 +431,64 @@ class Plansza () :
 						numerFragmentu += 1
 					
 					self.macierzSpriteów[y + sy][x + sx] = Fragment (numerFragmentu, x + sx, y + sy)
+					
+		pygame.mixer.Sound.play(uderzenieSFX)
 	
 	def RozbijLinie (self, linieDoRozbicia) :
 		
-		print ("rozbijanie linii")
-		
 		# odtwórz animację
 		
-		for _ in range (0, 60) :
+		spriteAnimacji = pygame.sprite.Group ()
+		
+		for numerLinii in linieDoRozbicia :
+			spriteAnimacji.add (AnimacjaRozbijanejLinii (len (linieDoRozbicia), numerLinii))
+		
+		global liczbaLinii
+		liczbaLinii += len (linieDoRozbicia)
+		
+		if len (linieDoRozbicia) == 1 :
+			pygame.mixer.Sound.play(cheemsSFX)
+			global liczbaLiniiCheems
+			liczbaLiniiCheems += 1
+		elif len (linieDoRozbicia) == 2 :
+			pygame.mixer.Sound.play(dogeSFX)
+			global liczbaLiniiDoge
+			liczbaLiniiDoge += 1
+		elif len (linieDoRozbicia) == 3 :
+			pygame.mixer.Sound.play(buffdogeSFX)
+			global liczbaLiniiBuffDoge
+			liczbaLiniiBuffDoge += 1
+		elif len (linieDoRozbicia) == 4 :
+			pygame.mixer.Sound.play(temtrisSFX)
+			global liczbaLiniiTemtris
+			liczbaLiniiTemtris += 1
+		
+		for x in range (0, 32) :
+			
+			# self.Rysuj ()
+			spriteAnimacji.draw (okno)
 			
 			pygame.display.update ()
-			zegar.tick (fps)
+			zegar.tick (fps * 2)
+			
+			for sprite in spriteAnimacji :
+				sprite.NastępnaKlatka ()
 		
 		# zmień grafiki
 		
 		for numerLinii in linieDoRozbicia :
 			if numerLinii - 1 >= 0 :
 				for x in range (0, 10) :
-					if self.macierzSpriteów [numerLinii][x] != None :
-						self.macierzSpriteów[numerLinii][x].UstawGrafikę (0, self.macierzSpriteów[numerLinii][x].numerFragmentu & 13)
+					if self.macierzSpriteów [numerLinii - 1][x] != None :
+						self.macierzSpriteów[numerLinii - 1][x].UstawGrafikę (0, self.macierzSpriteów[numerLinii - 1][x].numerFragmentu & 13)
 			
 			if numerLinii + 1 < 20 :
 				for x in range (0, 10) :
-					if self.macierzSpriteów [numerLinii][x] != None :
-						self.macierzSpriteów[numerLinii][x].UstawGrafikę (0, self.macierzSpriteów[numerLinii][x].numerFragmentu & 7)
+					if self.macierzSpriteów [numerLinii + 1][x] != None :
+						self.macierzSpriteów[numerLinii + 1][x].UstawGrafikę (0, self.macierzSpriteów[numerLinii + 1][x].numerFragmentu & 7)
 		
 		# usuń linie i utwórz nowe na górze
 		for numerLinii in linieDoRozbicia :
-			print ("usuń linię " + str (numerLinii))
 			del self.macierzSpriteów [numerLinii]
 			self.macierzSpriteów.insert (0, [])
 			for _ in range (0, 10) :
@@ -456,11 +538,25 @@ class Plansza () :
 		
 		print ()
 
+def OdtwarzajMuzykę () :
+	
+	if not pygame.mixer.music.get_busy () :
+		pygame.mixer.music.load (muzyka[randrange (4)])
+		pygame.mixer.music.play ()
+
 def Gra (dwóchGraczy) :
+	
+	if dwóchGraczy :
+		obecnyGracz = 1
+	else :
+		obecnyGracz = 0
 	
 	pygame.mixer.music.stop ()
 	
-	tłoGra = pygame.image.load ("Assets/Grafika/gra.png")
+	if dwóchGraczy :
+		tłoGra = pygame.image.load ("Assets/Grafika/gra dwóch graczy.png")
+	else :
+		tłoGra = pygame.image.load ("Assets/Grafika/gra.png")
 	okno.blit (tłoGra, tłoGra.get_rect())
 	
 	plansza = Plansza ()
@@ -488,43 +584,50 @@ def Gra (dwóchGraczy) :
 		
 		while opadanie :
 			
+			global zegarOpadania
+			global szybkośćOpadaniaKlocka
+			zegarOpadania -= 1
+			if zegarOpadania == 0 :
+				obecnyKlocek.Opadaj ()
+				zegarOpadania = szybkośćOpadaniaKlocka
+			
 			for event in pygame.event.get() :
 				
 				# naciśnięcie X na oknie
 				if event.type == QUIT:
 					pygame.quit ()
 					exit ()
-				
-				if event.type == KEYDOWN :
-					if event.key == A1 :
-						print ("A")
-						obecnyKlocek.ObróćWPrawo ()
-						
-					elif event.key == B1 :
-						print ("B")
-						obecnyKlocek.ObróćWLewo ()
-						
-					elif event.key == RIGHT1 :
-						print ("RIGHT")
-						obecnyKlocek.PrzesuńWPrawo ()
-						
-					elif event.key == DOWN1 :
-						print ("DOWN")
-						if not obecnyKlocek.Opadaj () :
-							opadanie = False
-						
-					elif event.key == LEFT1 :
-						print ("LEFT")
-						obecnyKlocek.PrzesuńWLewo ()
-						
-					print ("pozycja klocka (" + str (obecnyKlocek.x) + ", " + str (obecnyKlocek.y) + ")")
-					Klocek.plansza.DebugPokażMapeKolizji (obecnyKlocek.UtwórzMacierzKolizji(obecnyKlocek.image), obecnyKlocek.x, obecnyKlocek.y)
 			
-			# klawisze = pygame.key.get_pressed()  #checking pressed keys
-			# if keys[pygame.K_UP]:
-			# 	y1 -= 1
-			# if keys[pygame.K_DOWN]:
-			# 	y1 += 1
+			klawisze = pygame.key.get_pressed ()
+			
+			global zegarKontrolera
+			if zegarKontrolera > 0 :
+				zegarKontrolera -= 1
+			
+			# TODO jeśli naciśnięto to pierwszy raz powinien być dłuższy cooldown
+			if zegarKontrolera == 0 :
+				
+				if obecnyGracz == 0 and (klawisze [A1] or klawisze [A2]) or obecnyGracz == 1 and klawisze[A1] or obecnyGracz == 2 and klawisze[A2] :
+					obecnyKlocek.ObróćWPrawo ()
+					zegarKontrolera = 2
+				elif klawisze [B1] :
+					obecnyKlocek.ObróćWLewo ()
+					zegarKontrolera = 2
+				elif klawisze [DOWN1] :
+					if not obecnyKlocek.Opadaj () :
+						opadanie = False
+					zegarKontrolera = 2
+				
+				if klawisze [RIGHT1] :
+					obecnyKlocek.PrzesuńWPrawo ()
+					zegarKontrolera = 2
+				elif klawisze [LEFT1] :
+					obecnyKlocek.PrzesuńWLewo ()
+					zegarKontrolera = 2
+			
+			# Klocek.plansza.DebugPokażMapeKolizji (obecnyKlocek.UtwórzMacierzKolizji(obecnyKlocek.image), obecnyKlocek.x, obecnyKlocek.y)
+			
+			OdtwarzajMuzykę ()
 			
 			okno.blit (tłoGra, tłoGra.get_rect())
 			wszystkieSprite.draw (okno)
@@ -548,7 +651,7 @@ def Gra (dwóchGraczy) :
 		pygame.display.update ()
 		zegar.tick (fps)
 
-def KoniecGry () :
+def KoniecGry (dwóchGraczy) :
 	
 	while True :
 		
@@ -556,13 +659,16 @@ def KoniecGry () :
 		
 		# TODO czy da się to uprościć?
 		# czekaj 32 klatki
-		for _ in range (0, 16) :
+		for _ in range (0, 32) :
 			
 			pygame.display.update ()
 			zegar.tick (fps)
 		
 		koniecGryKlatka1 = pygame.image.load ("Assets/Grafika/koniec gry klatka 1.png")
-		koniecGryKlatka2 = pygame.image.load ("Assets/Grafika/koniec gry klatka 2.png")
+		if dwóchGraczy :
+			koniecGryKlatka2 = pygame.image.load ("Assets/Grafika/koniec gry klatka 2 dwóch graczy.png")
+		else :
+			koniecGryKlatka2 = pygame.image.load ("Assets/Grafika/koniec gry klatka 2.png")
 		
 		pygame.mixer.music.load ("Assets/Dźwięk/koniec gry.ogg")
 		pygame.mixer.music.play ()
@@ -602,4 +708,4 @@ while True :
 	
 	dwóchGraczy = Menu ()
 	Gra (dwóchGraczy)
-	KoniecGry ()
+	KoniecGry (dwóchGraczy)
